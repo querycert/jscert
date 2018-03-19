ab1ce8d1b30aa45b1787c5a0587b14fc177a7912
Jerome Simeon <simeon@us.ibm.com>
2018-03-18 23:27:59 -0400

    Re-align JsNumber module to existing JsCert one

--- coq/JsNumber.v
+++ coq/JsNumber.v
@@ -1,16 +1,19 @@
+(* Set Implicit Arguments. *)
+(* Require Export Shared. *)
 Require Flocq.Appli.Fappli_IEEE Flocq.Appli.Fappli_IEEE_bits.
+Require Import BinPos Zpower ZArith.
+Require Import String.
+
 
 (**************************************************************)
 (** ** Type for number (IEEE floats) *)
 
-Definition number : Set :=
+Definition number : Set := (* XXX Changed from Type, this may be significant for JsCert -JS *)
   Fappli_IEEE_bits.binary64.
 
 
 (**************************************************************)
 (** ** Particular values of numbers *)
-
-(** !!! B755_zero false = +0 ; B755_zero true = -0 *)
 
 Definition nan : number :=
   let (sign,nan) := Fappli_IEEE_bits.default_nan_pl64 in
@@ -27,62 +30,30 @@ Definition infinity : number :=
   Fappli_IEEE.B754_infinity _ _ false.
 Definition neg_infinity : number :=
   Fappli_IEEE.B754_infinity _ _ true.
-
-Require Import BinPos.
-Require Import Zpower.
-Require Import ZArith.
-
 Definition max_value : number :=
-  Fappli_IEEE.B754_finite 53 1024 false ((shift_pos 53 1)-1)%positive (1024-53)%Z (eq_refl true). (* *)
+  Fappli_IEEE.B754_finite 53 1024 false ((shift_pos 53 1)-1)%positive (1024-53)%Z (eq_refl true).
 Definition min_value : number :=
    Fappli_IEEE.B754_finite 53 1024 false 1 (-1074) (eq_refl true). (* 1x2e1-1074 *)
+Parameter pi : number.
+Parameter e : number.
+Parameter ln2 : number.
 
 (**************************************************************)
 (** ** Conversions on numbers *)
 
-Require Import String.
-Definition float_triple : Set := (bool * positive * Z).
-Definition float_triple_to_b64 (ft:float_triple) : number :=
-  let '(sign, m, e) := ft in
-  match e with
-  | Z0 => Fappli_IEEE.B754_zero _ _ false
-  | Zneg e =>
-    let my :=
-        match Zpower_pos 10 e with
-        | Zpos p => p
-        | _ => xH
-        end
-    in
-    Fappli_IEEE.FF2B
-      53 1024 _
-      (proj1
-         (Fappli_IEEE.Bdiv_correct_aux
-            53 1024
-            (eq_refl Lt) (eq_refl Lt)
-            Fappli_IEEE.mode_NE
-            sign m 0
-            false my 0))
-  | Zpos e =>
-    Fappli_IEEE.B754_zero _ _ false
-  end.
-
+(* LATER: implement definitions *)
 Parameter from_string : string -> number.
 Parameter to_string : number -> string.
 
 (**************************************************************)
 (** ** Unary operations on numbers *)
 
-(* LATER: find definitions in Flocq *)
-
-Definition number_neg : number -> number :=
+Definition neg : number -> number :=
   Fappli_IEEE_bits.b64_opp.
-Parameter number_floor : number -> number.  (** Note: not in flocq maybe in the longer future *)
-Definition number_b64_abs : number -> number :=
+Parameter floor : number -> number.  (* Note: not in flocq maybe in the future *)
+Definition absolute : number -> number :=
   Fappli_IEEE.Babs 53 1024 pair.
-Definition number_absolute : number -> number := number_b64_abs.
-
-(* This sign function is consistent with Math.sign in JavaScript *)
-Definition number_sign : number -> number :=
+Definition sign : number -> number := (* Note: This sign function is consistent with Math.sign in JavaScript *)
   fun x => match x with
   | Fappli_IEEE.B754_nan _ _ s _ => x
   | Fappli_IEEE.B754_zero _ _ s => x
@@ -92,85 +63,70 @@ Definition number_sign : number -> number :=
     then neg_one
     else one
   end.
+Definition lt_bool (n1 n2 : number) : bool :=
+  match Fappli_IEEE.Bcompare _ _ n1 n2 with
+  | Some Lt => true
+  | Some _ => false
+  | None => false (* Note: This is for nan, always false, except for not equal *)
+  end.
+
 
 (**************************************************************)
 (** ** Binary operations on numbers *)
 
-Definition number_add : number -> number -> number :=
+Definition add : number -> number -> number :=
   Fappli_IEEE_bits.b64_plus Fappli_IEEE.mode_NE.
 
-Definition number_sub : number -> number -> number :=
+Definition sub : number -> number -> number :=
   Fappli_IEEE_bits.b64_minus Fappli_IEEE.mode_NE.
 
-Parameter number_mod : number -> number -> number. (** Note: may come in next version of Flocq *)
+Parameter fmod : number -> number -> number. (* Note: may come in next version of Flocq *)
 
-Definition number_mult : number -> number -> number :=
+Definition mult : number -> number -> number :=
   Fappli_IEEE_bits.b64_mult Fappli_IEEE.mode_NE.
 
-Definition number_div : number -> number -> number :=
+Definition div : number -> number -> number :=
   Fappli_IEEE_bits.b64_div Fappli_IEEE.mode_NE.
 
-Require Import List.
-(** Defines additional operations on FLOATs *)
-(** Unary operations *)
+(* Todo: find comparison operator *)
+(* Global Instance number_comparable : Comparable number.
+Proof. Admitted. *)
 
-Parameter number_sqrt : number -> number. (** In Flocq *)
-Parameter number_exp : number -> number.
-Parameter number_log : number -> number.
-Parameter number_log10 : number -> number.
-Parameter number_ceil : number -> number.
-Parameter number_eq : number -> number -> bool. (* TODO by Jerome pattern matching on B754 *)
+(**************************************************************)
+(** ** Conversions with Int32 *)
 
-Conjecture number_eq_correct :
-  forall f1 f2, (number_eq f1 f2 = true <-> f1 = f2).
+(* Parameter of_int : int -> number. (* LATER: this is quite complex. Should we make it precise? *) *)
 
-Require Import EquivDec.
-Lemma number_eq_dec : EqDec number eq.
-Proof.
-  unfold EqDec.
-  intros x y.
-  case_eq (number_eq x y); intros eqq.
-  + left.
-    f_equal.
-    apply number_eq_correct in eqq.
-    trivial.
-  + right; intros eqq2.
-    red in eqq2.
-    apply number_eq_correct in eqq2.
-    congruence.
-Defined.
-  
-Parameter number_pow : number -> number -> number.
-Parameter number_min : number -> number -> number. (** Check in JS spec what happens for min/max *)
-Parameter number_max : number -> number -> number.
-Parameter number_ne : number -> number -> bool.
-Definition number_lt (n1 n2 : number) :=
-  match Fappli_IEEE.Bcompare _ _ n1 n2 with
-  | Some Lt => true
-  | Some _ => false
-  | None => false (**r None is for nan, always false, except for not equal *)
-  end.
-Parameter number_le : number -> number -> bool.
-Parameter number_gt : number -> number -> bool.
-Parameter number_ge : number -> number -> bool.
+(* Parameter to_int32 : number -> int. (* Remark: extracted code could, for efficiency reasons, use Ocaml Int32 *)  *)
 
-Require Import ZArith.
-Parameter number_of_int : Z -> number. (** Binary normalize *)
-Parameter number_truncate : number -> Z. (** Do like parsing ... *)
+(* Parameter to_uint32 : number -> int. *)
 
-Definition number_list_min (l:list number) : number :=
-  fold_right (fun x y => number_min x y) infinity l.
+(* Parameter to_int16 : number -> int. (* currently not used *) *)
 
-Definition number_list_max (l:list number) : number :=
-  fold_right (fun x y => number_max x y) neg_infinity l.
+(* LATER: Check that the OCaml extraction is correct. *)
 
-Definition number_list_sum (l:list number) : number :=
-  fold_right (fun x y => number_add x y) zero l.
 
-Definition number_list_arithmean (l:list number) : number :=
-  let ll := List.length l in
-  match ll with
-  | O => zero
-  | _ => number_div (number_list_sum l) (number_of_int (Z_of_nat ll))
-  end.
+(**************************************************************)
 
+(** Implements the operation that masks all but the 5 least significant bits
+   of a non-negative number (obtained as the result of to_uint32 *)
+
+(* Parameter modulo_32 : int -> int. *)
+
+(** Implements int32 operation *)
+
+(* Parameter int32_bitwise_not : int -> int. *)
+
+(* Parameter int32_bitwise_and : int -> int -> int. *)
+(* Parameter int32_bitwise_or : int -> int -> int. *)
+(* Parameter int32_bitwise_xor : int -> int -> int. *)
+
+(* Parameter int32_left_shift : int -> int -> int. *)
+(* Parameter int32_right_shift : int -> int -> int. *)
+(* Parameter uint32_right_shift : int -> int -> int. *)
+
+
+
+
+(**************************************************************)
+(** ** Int32 related conversion *)
