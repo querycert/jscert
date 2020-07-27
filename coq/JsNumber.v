a960697db47292121d089e836083eed2b857acb7
Avi Shinnar <shinnar@us.ibm.com>
2020-07-26 20:11:07 -0400

    Update to use Coq 8.11.2 and Coq native floats.  Removes flocq dependency.

--- coq/JsNumber.v
+++ coq/JsNumber.v
@@ -1,6 +1,6 @@
 (* Set Implicit Arguments. *)
 (* Require Export Shared. *)
-Require Flocq.Appli.Fappli_IEEE Flocq.Appli.Fappli_IEEE_bits.
+Require Floats PrimFloat.
 Require Import BinPos Zpower ZArith.
 Require Import String.
 
@@ -9,31 +9,27 @@ Require Import String.
 (** ** Type for number (IEEE floats) *)
 
 Definition number : Set := (* XXX Changed from Type, this may be significant for JsCert -JS *)
-  Fappli_IEEE_bits.binary64.
+  PrimFloat.float.
 
 
 (**************************************************************)
 (** ** Particular values of numbers *)
 
-Definition nan : number :=
-  let (sign,nan) := Fappli_IEEE_bits.default_nan_pl64 in
-  Fappli_IEEE.B754_nan _ _ sign nan.
-Definition zero : number :=
-  Fappli_IEEE.B754_zero _ _ false.
-Definition neg_zero : number :=
-  Fappli_IEEE.B754_zero _ _ true.
-Definition one :=
-  Fappli_IEEE.binary_normalize 53 1024 eq_refl eq_refl Fappli_IEEE.mode_NE 1 0 false.
-Definition neg_one :=
-  Fappli_IEEE.binary_normalize 53 1024 eq_refl eq_refl Fappli_IEEE.mode_NE 1 0 true.
-Definition infinity : number :=
-  Fappli_IEEE.B754_infinity _ _ false.
-Definition neg_infinity : number :=
-  Fappli_IEEE.B754_infinity _ _ true.
+Definition nan : number := PrimFloat.nan.
+Definition zero : number := PrimFloat.zero.
+Definition neg_zero : number := PrimFloat.neg_zero.
+Definition one := PrimFloat.one.
+Definition neg_one := Eval compute in (PrimFloat.opp PrimFloat.one).
+Definition infinity : number := PrimFloat.infinity.
+Definition neg_infinity : number := PrimFloat.neg_infinity.
+
+(*
 Definition max_value : number :=
   Fappli_IEEE.B754_finite 53 1024 false ((shift_pos 53 1)-1)%positive (1024-53)%Z (eq_refl true).
 Definition min_value : number :=
    Fappli_IEEE.B754_finite 53 1024 false 1 (-1074) (eq_refl true). (* 1x2e1-1074 *)
+ *)
+
 Parameter pi : number.
 Parameter e : number.
 Parameter ln2 : number.
@@ -48,45 +44,31 @@ Parameter to_string : number -> string.
 (**************************************************************)
 (** ** Unary operations on numbers *)
 
-Definition neg : number -> number :=
-  Fappli_IEEE_bits.b64_opp.
+Definition neg : number -> number := PrimFloat.opp.
 Parameter floor : number -> number.  (* Note: not in flocq maybe in the future *)
-Definition absolute : number -> number :=
-  Fappli_IEEE.Babs 53 1024 pair.
-Definition sign : number -> number := (* Note: This sign function is consistent with Math.sign in JavaScript *)
-  fun x => match x with
-  | Fappli_IEEE.B754_nan _ _ s _ => x
-  | Fappli_IEEE.B754_zero _ _ s => x
-  | Fappli_IEEE.B754_infinity _ _ s => x
-  | Fappli_IEEE.B754_finite _ _ s _ _ _ =>
-    if s
-    then neg_one
-    else one
-  end.
-Definition lt_bool (n1 n2 : number) : bool :=
-  match Fappli_IEEE.Bcompare _ _ n1 n2 with
-  | Some Lt => true
-  | Some _ => false
-  | None => false (* Note: This is for nan, always false, except for not equal *)
-  end.
+Definition absolute : number -> number :=  PrimFloat.abs.
 
+Definition sign : number -> number := (* Note: This sign function is consistent with Math.sign in JavaScript *)
+  fun x => match PrimFloat.classify x with
+        | FloatClass.PNormal => one
+        | FloatClass.NNormal => neg_one
+        | _ => x
+        end.
+
+Definition lt_bool : number -> number -> bool := PrimFloat.ltb.
 
 (**************************************************************)
 (** ** Binary operations on numbers *)
 
-Definition add : number -> number -> number :=
-  Fappli_IEEE_bits.b64_plus Fappli_IEEE.mode_NE.
+Definition add : number -> number -> number := PrimFloat.add.
 
-Definition sub : number -> number -> number :=
-  Fappli_IEEE_bits.b64_minus Fappli_IEEE.mode_NE.
+Definition sub : number -> number -> number := PrimFloat.sub.
 
-Parameter fmod : number -> number -> number. (* Note: may come in next version of Flocq *)
+Parameter fmod : number -> number -> number. (* Note: may come in next version of float *)
 
-Definition mult : number -> number -> number :=
-  Fappli_IEEE_bits.b64_mult Fappli_IEEE.mode_NE.
+Definition mult : number -> number -> number := PrimFloat.mul.
 
-Definition div : number -> number -> number :=
-  Fappli_IEEE_bits.b64_div Fappli_IEEE.mode_NE.
+Definition div : number -> number -> number := PrimFloat.div.
 
 (* Todo: find comparison operator *)
 (* Global Instance number_comparable : Comparable number.
